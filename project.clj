dde17e1fb545eee9e0c62b8dbf91a1d8b37b3bef
Robert Schrrag <bob.schrag@gmail.com>
2023-07-29 07:39:47 -0700

    Update version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clolog "0.1.11"
+(defproject clolog "0.2.0"
   :description "Full-featured logic programming (AKA "Prolog") embedded in/callable from and supporting calls to Clojure."
   :url "https://github.com/bobschrag/clolog/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
